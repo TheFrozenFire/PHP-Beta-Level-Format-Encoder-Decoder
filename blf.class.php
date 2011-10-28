6c313092965595c27d3ddfbde0b9db08def101a6
TheFrozenFire <frozenfire@thefrozenfire.com>
2011-10-28 16:43:30 -0700

    Fixed last commit. Forgot to add NBT + blf.class.php

--- blf.class.php
+++ blf.class.php
@@ -10,18 +10,32 @@
  *  NBT class (https://github.com/TheFrozenFire/PHP-NBT-Decoder-Encoder)
  */
 class BLF {
-	public $header;
+	public $verbose = false;
+	public $header = array(); // Array ( "offset" => int, "size" => int, "time" => int, "chunk" => NBT::root[] )
 
 	public function loadFile($filename, $readall = true) {
+		if($this->verbose) trigger_error("Loading file \"{$filename}\".", E_USER_NOTICE);
 		$fp = fopen($filename, "rb");
+		if($this->verbose) trigger_error("Loading header from file.", E_USER_NOTICE);
 		$this->readHeader($fp);
-		if($readall) foreach($this->header as $location) $this->readChunk($fp, $location); else return $fp;
+		if($readall) {
+			if($this->verbose) trigger_error("Loading all chunks.", E_USER_NOTICE);
+			foreach($this->header as &$location) $this->readChunk($fp, $location);
+		} else {
+			if($this->verbose) trigger_error("Returning file resource.", E_USER_NOTICE);
+			return $fp;
+		}
 	}
 	
 	public function writeFile($filename) {
+		if($this->verbose) trigger_error("Writing ".count($this->header)." chunks to \"{$filename}\".", E_USER_NOTICE);
 		$fp = fopen($filename, "wb");
 		fseek($fp, 8192);
-		foreach($header as $location) if(!$this->writeChunk($fp, $location)) return false;
+		foreach($this->header as $locationNum => &$location) if(!$this->writeChunk($fp, $location)) {
+			trigger_error("Failed to write chunk #{$locationNum}.", E_USER_WARNING);
+			return false;
+		}
+		if($this->verbose) trigger_error("Writing header to file.", E_USER_NOTICE);
 		$this->writeHeader($fp);
 		return true;
 	}
@@ -33,19 +47,22 @@ class BLF {
 			fseek($fp, $headerItem * 4);
 			list(,$location["offset"]) = unpack("N", "\0".fread($fp, 3)); // Offset in file
 			if($location["offset"] == 0) continue; // Chunk isn't present
-			list(,$location["size"]) = unpack("c", fread($fp, 1)); // Size
+			if($this->verbose) trigger_error("Reading header item starting at offset ".($headerItem*4).".", E_USER_NOTICE);
+			list(,$location["size"]) = unpack("C", fread($fp, 1)); // Size
 			fseek($fp, ($headerItem * 4) + 4096);
 			list(,$location["time"]) = unpack("N", fread($fp, 4)); // Modification timestamp
-			$this->header[$headerItem] = $location;
+			if($this->verbose) trigger_error("Header item {\"offset\"=>{$location["offset"]}, \"size\"=>{$location["size"]}, \"time\"=>{$location["time"]}}", E_USER_NOTICE);
+			$this->header[] = $location;
 		}
 	}
 	
 	public function writeHeader($fp) {
 		foreach($this->header as $key => $location) {
+			if($this->verbose) trigger_error("Header item #{$key} being written at offset ".($key * 4).", with timestamp at ".($key * 4 + 4096)." {\"offset\"=>{$location["offset"]}, \"size\"=>{$location["size"]}, \"time\"=>{$location["time"]}}", E_USER_NOTICE);
 			fseek($fp, $key * 4);
-			fwrite($fp, substr(pack("N", $location["offset"]), 0, 3)); // Offset in file
-			fwrite($fp, pack("c", $location["size"])); // Size
-			fseek($fp, ($key * 4) + 4096);
+			fwrite($fp, substr(pack("N", $location["offset"]), 1, 3)); // Offset in file
+			fwrite($fp, pack("C", $location["size"])); // Size
+			fseek($fp, $key * 4 + 4096);
 			fwrite($fp, pack("N", $location["time"])); // Modification timestamp
 		}
 	}
@@ -54,6 +71,8 @@ class BLF {
 		fseek($fp, $location["offset"]*4096);
 		list(,$length) = unpack("N", fread($fp, 4));
 		list(,$compression) = unpack("c", fread($fp, 1));
+		
+		if($this->verbose) trigger_error("Reading chunk at offset ".($location["offset"]*4096)." of {$length} bytes using compression #{$compression}.", E_USER_NOTICE);
 
 		$data = gzuncompress(stream_get_contents($fp, $length));
 		$temp = fopen("php://temp", "r+");
@@ -66,11 +85,12 @@ class BLF {
 	}
 	
 	public function writeChunk($fp, &$location) {
-		$location["offset"] = ftell($fp) / 4096;
+		$location["offset"] = floor(ftell($fp) / 4096);
+		if($this->verbose) trigger_error("Chunk is being written at offset ".ftell($fp).".", E_USER_NOTICE);
 		fseek($fp, 4, SEEK_CUR);
 
 		$start = ftell($fp);
-		fwrite(pack("c", 2));
+		fwrite($fp, pack("c", 2));
 
 		$temp = fopen("php://temp", "r+");
 		$nbt = new NBT();
@@ -80,12 +100,13 @@ class BLF {
 		fclose($temp);
 		fwrite($fp, $compressed);
 
-		$size = ftell($fp)-$start+1;
+		$size = ftell($fp)-($start+1);
 		fseek($fp, $start-4);
-		fwrite(pack("N", $size));
+		fwrite($fp, pack("N", $size));
 
 		$location["size"] = ceil($size / 4096);
-		fseek($fp, $location["offset"] + ($location["size"] * 4096));
+		fseek($fp, ($location["offset"] * 4096) + ($location["size"] * 4096));
+		return true;
 	}
 }
 ?>
